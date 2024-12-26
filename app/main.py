08cbdfb32b4bf337c8293784b913e0f880b01b5a
MATUMOTO Eugene <yuu.mat.930@gmail.com>
2024-12-26 07:07:00 +0000

    add routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
-
-from app.model.grocery import ItemPayload
+from models.grocery import ItemPayload
 
 app = FastAPI()
 
@@ -10,10 +9,7 @@ grocery_list: dict[int, ItemPayload] = {}
 @app.post("/items/{name}/{quantity}")
 def add_item(name: str, quantity: int) -> dict[str, ItemPayload]:
     if quantity <= 0:
-        raise HTTPException(
-            status_code=400,
-            detail="Quantity must be greater than 0"
-        )
+        raise HTTPException(status_code=400, detail="Quantity must be greater than 0")
 
     # if item already exists, we'll just add the quantity.
     # get all item names
@@ -24,51 +20,45 @@ def add_item(name: str, quantity: int) -> dict[str, ItemPayload]:
 
     if name in items_ids.keys():
         item_id = items_ids[name]
-        grocery_list[item_id] = ItemPayload(
-            id=item_id,
-            name=name,
-            quantity=quantity
-        )
+        grocery_list[item_id] = ItemPayload(id=item_id, name=name, quantity=quantity)
     else:
         item_id = max(grocery_list.keys()) + 1 if grocery_list else 1
-        grocery_list[item_id] = ItemPayload(
-            id=item_id,
-            name=name,
-            quantity=quantity
-        )
+        grocery_list[item_id] = ItemPayload(id=item_id, name=name, quantity=quantity)
 
     return {"item": grocery_list[item_id]}
 
 
+@app.get("/items/{id}")
+def get_item(id: int) -> dict[str, ItemPayload]:
+    if id not in grocery_list:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return {"item": grocery_list[id]}
+
+
 @app.get("/items")
-def list_items(id: int) -> dict[str, ItemPayload]:
-    return {"items": grocery_list[id]}
+def list_items() -> dict[str, dict[int, ItemPayload]]:
+    return {"items": grocery_list}
 
 
 @app.delete("/items/{id}")
-def delete_item(id: int):
+def delete_item(id: int) -> dict[str, str]:
     if id not in grocery_list:
-        raise HTTPException(
-            status_code=404,
-            detail="Item not found"
-        )
+        raise HTTPException(status_code=404, detail="Item not found")
 
     del grocery_list[id]
     return {"message": "Item deleted successfully"}
 
 
 @app.delete("/items/{id}/{quantity}")
-def remove_quantity(id: int, quantity: int):
+def remove_quantity(id: int, quantity: int) -> dict[str, str]:
     if id not in grocery_list:
-        raise HTTPException(
-            status_code=404,
-            detail="Item not found"
-        )
+        raise HTTPException(status_code=404, detail="Item not found")
 
     if grocery_list[id].quantity <= quantity:
         raise HTTPException(
             status_code=400,
-            detail="Quantity to remove is greater than the available quantity"
+            detail="Quantity to remove is greater than the available quantity",
         )
     else:
         grocery_list[id].quantity -= quantity
